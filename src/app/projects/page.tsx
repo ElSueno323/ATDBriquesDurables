2df84c142a26c54ee9af42985e8dad2f5cc886e8
Gabriel Espinosa <54720@etu.he2b.be>
2025-06-24 19:38:05 +0200

    Ajout du composant ProjectsLatest à la page des projets et mise à jour des styles CSS pour la section des derniers projets. Intégration des traductions en anglais et en français pour les titres et descriptions des projets récents.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import TitleProjet from './TitleProjet';
 import ProjectsHeader from './ProjectsHeader';
 import ProjectFeatureSection from './ProjectFeatureSection';
+import ProjectsLatest from './ProjectsLatest';
 
 export default function ProjectsPage() {
   return (
@@ -8,6 +9,7 @@ export default function ProjectsPage() {
       <TitleProjet />
       <ProjectsHeader />
       <ProjectFeatureSection />
+      <ProjectsLatest />
     </>
   );
 } 
